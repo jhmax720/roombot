047eb126e16c88dcde852fcdcc613f47f0655e88
Maxwell jiang <maxpriv22@hotmail.com>
2019-03-24 21:18:55 +1100

    test

--- routes/index.js
+++ routes/index.js
@@ -4,28 +4,48 @@ var express = require('express');
 var router = express.Router();
 const { Wechaty } = require('wechaty')
 
+async function delayedMessage(room)
+{
+  if(room.payload.topic && room.payload.topic.toLowerCase().indexOf('mybeauty') === -1)
+  {
+    console.log('processed room: '+ room.payload.topic)
+    await room.say('<img class="emoji emoji1f338" text="_web" src="/zh_CN/htmledition/v2/images/spacer.gif"><img class="emoji emoji1f44f" text="_web" src="/zh_CN/htmledition/v2/images/spacer.gif">墨尔本东区美容院，三月特价新鲜出炉啦！<img class="emoji emoji1f338" text="_web" src="/zh_CN/htmledition/v2/images/spacer.gif"><br><img class="emoji emoji1f338" text="_web" src="/zh_CN/htmledition/v2/images/spacer.gif">一，韩式半永久纹眉仅需$250，美瞳线仅$200！美甲纯色任选仅45<img class="emoji emoji1f485" text="_web" src="/zh_CN/htmledition/v2/images/spacer.gif"><br><img class="emoji emoji1f338" text="_web" src="/zh_CN/htmledition/v2/images/spacer.gif">二，日式纯氧翘睫术—本月仅需$108，可维持40-60天。<br><img class="emoji emoji1f338" text="_web" src="/zh_CN/htmledition/v2/images/spacer.gif">三，王牌项目“秘制祛痘”疗程，原价$1980/8次现$1200/8次（我们是包去掉痘痘，无效全额🙈退款）<br><img class="emoji emoji1f338" text="_web" src="/zh_CN/htmledition/v2/images/spacer.gif">四，台式—嫁接睫毛，本月仅$88/次（保证你的睫毛又长又浓密哦～）<br><img class="emoji emoji1f338" text="_web" src="/zh_CN/htmledition/v2/images/spacer.gif">五，产后修复SPA—养生系妈咪可以来体验—恢复少女身材，原价$398，本月仅$158！<br><img class="emoji emoji1f338" text="_web" src="/zh_CN/htmledition/v2/images/spacer.gif">六，韩国进口—水光针原价$760/次，本月仅$280！做一次维持大半年！<br>有兴趣的宝宝➕微信预约啦～只要你来问都给你优惠价！<img class="emoji emoji1f389" text="_web" src="/zh_CN/htmledition/v2/images/spacer.gif"><img class="emoji emoji1f338" text="_web" src="/zh_CN/htmledition/v2/images/spacer.gif">');  
+  }
+  else
+  {
+    console.log('unable to process room: '+ room.payload.topic)
+  }
+  
+}
+
+async function initRoom() {
+  console.log('begin to initRoom!')
+  const roomList = await bot.Room.findAll()
+  if (roomList.length < 100) {
+    console.warn(`room num not ready. retry after 10 seconds. room num: ${roomList.length}`)
+    setTimeout(initRoom, 10000)
+    return
+  }
+  return roomList
+}
 
 async function broadcast() {
-  const roomList = await bot.Room.findAll()                    // get the room list of the bot
+  const roomList = await initRoom();                   // get the room list of the bot
 
   console.log('current index: ' + msgsentCount);
-  roomList.forEach(x => {
 
-    if(x.payload.topic && x.payload.topic.toLowerCase().indexOf('mybeauty') === -1)
-    {
+  console.log('room count: '+ roomList.length);
+  const promises = roomList.map(delayedMessage);
+  // wait until all promises are resolved
+  await Promise.all(promises);
 
-      if(x.payload.topic.toLowerCase().includes('testing'))
-      {
-        console.log(x);
-        x.say('<img class="emoji emoji1f389" text="_web" src="/zh_CN/htmledition/v2/images/spacer.gif"><img class="emoji emoji1f389" text="_web" src="/zh_CN/htmledition/v2/images/spacer.gif"><img class="emoji emoji1f389" text="_web" src="/zh_CN/htmledition/v2/images/spacer.gif">墨尔本东区美容院，三月特价新鲜出炉啦～<br>一，韩式半永久眉毛$250，美瞳线仅需$200！美甲纯色仅30<br>二，胸部乳腺疏通原价$398/次，本月仅需$158！<br>三，秘制祛痘疗程，原价$1980/8次现$1200/8次（我们是包去掉痘痘，无效全额🙈退款）<br>四，台式嫁接睫毛，本月仅$88/次（保证你的睫毛又长又浓密）<br>五，肩颈理疗SPA—养生系宝可以来感受，原价$398，本月仅$128！<br>六，韩国进口水光针原价$760/次，本月仅$280！做一次维持大半年～<br>有兴趣的宝宝➕微信预约啦～只要你来问都给你优惠价！<img class="emoji emoji1f389" text="_web" src="/zh_CN/htmledition/v2/images/spacer.gif"><img class="emoji emoji1f389" text="_web" src="/zh_CN/htmledition/v2/images/spacer.gif"><img class="emoji emoji1f389" text="_web" src="/zh_CN/htmledition/v2/images/spacer.gif"><img class="emoji emoji1f389" text="_web" src="/zh_CN/htmledition/v2/images/spacer.gif"><img class="emoji emoji1f389" text="_web" src="/zh_CN/htmledition/v2/images/spacer.gif"><img class="emoji emoji1f389" text="_web" src="/zh_CN/htmledition/v2/images/spacer.gif">');
-        // x.say('1');
-      }
-      
-    }
-    //if(x.to)
-    
-    //x.say('<img class="emoji emoji1f389" text="_web" src="/zh_CN/htmledition/v2/images/spacer.gif"><img class="emoji emoji1f389" text="_web" src="/zh_CN/htmledition/v2/images/spacer.gif"><img class="emoji emoji1f389" text="_web" src="/zh_CN/htmledition/v2/images/spacer.gif">墨尔本东区美容院，三月特价新鲜出炉啦～<br>一，韩式半永久眉毛$250，美瞳线仅需$200！美甲纯色仅30<br>二，胸部乳腺疏通原价$398/次，本月仅需$158！<br>三，秘制祛痘疗程，原价$1980/8次现$1200/8次（我们是包去掉痘痘，无效全额🙈退款）<br>四，台式嫁接睫毛，本月仅$88/次（保证你的睫毛又长又浓密）<br>五，肩颈理疗SPA—养生系宝可以来感受，原价$398，本月仅$128！<br>六，韩国进口水光针原价$760/次，本月仅$280！做一次维持大半年～<br>有兴趣的宝宝➕微信预约啦～只要你来问都给你优惠价！<img class="emoji emoji1f389" text="_web" src="/zh_CN/htmledition/v2/images/spacer.gif"><img class="emoji emoji1f389" text="_web" src="/zh_CN/htmledition/v2/images/spacer.gif"><img class="emoji emoji1f389" text="_web" src="/zh_CN/htmledition/v2/images/spacer.gif"><img class="emoji emoji1f389" text="_web" src="/zh_CN/htmledition/v2/images/spacer.gif"><img class="emoji emoji1f389" text="_web" src="/zh_CN/htmledition/v2/images/spacer.gif"><img class="emoji emoji1f389" text="_web" src="/zh_CN/htmledition/v2/images/spacer.gif">');
-  })
+
+
+
+  console.log('Done!');
+
+
+  
 }
 
 /* GET home page. */
@@ -51,7 +71,7 @@ router.get('/fire', async function (req, res, next) {
       try {
         await broadcast();
       }
-      catch{
+      catch(err) {
         //DONT CARE
       }
 
